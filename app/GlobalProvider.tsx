dd867e1361a024a94d3bcf2235015f1f6a9d1a6e
Mohak-Tripathi <mohaktripathi029@gmail.com>
2023-10-25 12:16:08 +0530

    chapter 31- no code added explained server compoennt and client component

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "react-hot-toast";
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Toaster />   //GlobalProvider is the top level. So added here
+      <Toaster />   //GlobalProvider is the top level. So added here.
       {children} 
     </>
   );
