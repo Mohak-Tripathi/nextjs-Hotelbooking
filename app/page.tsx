05e65304807ae326a48596d2b9e660008a3c3916
Mohak-Tripathi <mohaktripathi029@gmail.com>
2023-10-25 15:53:08 +0530

    chapter 32-34- DATA FETCHING IN PAGE.TSX. Also //read about data fetching, data caching and data revalidating and before that server components, client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import Home from "@/components/Home";
 
-export default function HomePage() {
+// export const dynamic = "force-dynamic";
+
+//read about data fetching, data caching and data revalidating and before that server components, client components.
+const getRooms = async () => {
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    next: {
+      tags: ["Rooms"],
+    },
+  });
+  return res.json();
+};
+
+export default async function HomePage() {
+  const rooms = await getRooms();
+  console.log("resPerPage => ", rooms.resPerPage);
+
   return <Home />;
 }
