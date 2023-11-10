837bfcffb71f809ba7565d572a1ad4a60a816058
Mohak-Tripathi <mohaktripathi029@gmail.com>
2023-11-10 21:03:20 +0530

    ch50 added register user logic. Created auth/Register.tsx in components folder. Also in redux, added api/authApi.ts, added this in store.js, created router in app folder

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,26 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from "./features/userSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: userReducer,
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
+import { authApi } from "./api/authApi";
 
 export const store = configureStore({
   reducer: {
     auth: userReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([authApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
