e5caa00f5f0763b6e6d1e30d5d576b28c45df062
Mohak-Tripathi <mohaktripathi029@gmail.com>
2023-11-11 07:46:50 +0530

    ch53- added compoennets/user/UpdateProfile.tsx, app/me/update/page.tsx and redux/api/userApi.ts

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      API_URL: "http://localhost:3000",
-      DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
-      DB_URI: "",
-    },
+  env: {
+    API_URL: "http://localhost:3000",
+    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
+    DB_URI: "",
+
+    CLOUDINARY_CLOUD_NAME: "",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
+
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_SECRET: "KSDFJKLSDJFLKSDFJSLDKF934KJLDJGDLKGFJDF",
+  },
     images: {
       domains: ["res.cloudinary.com"],
     },
   };
   
   module.exports = nextConfig;
+
+
+
